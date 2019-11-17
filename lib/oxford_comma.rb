68fce0fb562f55e3af8dbb06bda6cb01130004a8
Stela Capsa <stela.capsa@gmail.com>
2019-11-17 23:53:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  
+ 
+  if array.length == 2
+    return array.join(" and ")
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  
+  return array.join(', ')
 end
